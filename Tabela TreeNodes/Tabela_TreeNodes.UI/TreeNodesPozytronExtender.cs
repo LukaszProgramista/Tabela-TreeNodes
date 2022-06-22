f03135fcee54ac054e0fd1e8968de78149124d29
LukaszProgramista <lukaszglowacki112@gmail.com>
2022-06-22 22:39:20 +0200

    Dodaj pliki projektów.

---
+++ Tabela TreeNodes/Tabela_TreeNodes.UI/TreeNodesPozytronExtender.cs
@@ -0,0 +1,37 @@
+﻿using Soneta.Business;
+using Soneta.CRM;
+using Soneta.Handel;
+using System.Linq;
+using Tabela_TreeNodes.UI;
+
+[assembly: Worker(typeof(TreeNodesPozytronExtender))]
+namespace Tabela_TreeNodes.UI
+{
+    public class TreeNodesPozytronExtender
+    {
+        [Context]
+        public Session Session { get; set; }
+
+        [Context]
+        public Kontrahent Contractor { get; set; }
+
+        public View Localizations() =>
+           Contractor.Lokalizacje.CreateView(); 
+
+        public View Devices()
+            =>
+            Contractor.Urzadzenia.CreateView();                    
+
+        public View Contracts()
+        {
+            var hm = Session.GetHandel();
+            var contracts = hm.DokHandlowe.WgDaty
+                .Where(d =>
+                    (d.Definicja == hm.DefDokHandlowych.UmowaCykliczna ||
+                    d.Definicja == hm.DefDokHandlowych.AneksCyklicznej) &&
+                    d.Kontrahent == Contractor
+                ).ToList();
+            return new View(hm.DokHandlowe.PrimaryKey, contracts);
+        }
+    }
+}
